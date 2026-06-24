2a550e2aca51ad4964fd180111ebb2d2aacf0dc8
jularis <juliuskouame@gmail.com>
2026-06-24 17:09:55 +0000

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -133,10 +133,6 @@
                 </a>
 
                 {{-- RDV --}}
-                <a href="#" class="hidden md:flex items-center gap-1.5 text-xs font-medium text-gray-600 hover:text-navy transition-colors px-2 py-1.5 rounded hover:bg-gray-50">
-                    <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"/></svg>
-                    Prendre RDV
-                </a>
 
                 {{-- Account --}}
                 @auth
